27e90177f7dec2a5088961858e148e114c4e4c43
Hyago Gabriel <oicueio616@gmail.com>
2022-01-25 11:16:26 -0300

    Página de 'Login' reage a existencia de um User

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,8 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
 import appConfig from "../../config.json";
-import QRcode from "assets/svg/qrcode.svg";
-
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import axios from "axios";
 
 function Titulo(props) {
   const Tag = props.tag || "h1";
@@ -61,11 +32,83 @@ function Titulo(props) {
 // export default HomePage;
 
 export default function PaginaInicial() {
-  const username = "hyagohoudini";
+  const defautlUser = {
+    login: null,
+    id: 0,
+    node_id: null,
+    avatar_url: null,
+    gravatar_id: null,
+    url: null,
+    html_url: null,
+    followers_url: null,
+    following_url: null,
+    gists_url: null,
+    starred_url: null,
+    subscriptions_url: null,
+    organizations_url: null,
+    repos_url: null,
+    events_url: null,
+    received_events_url: null,
+    type: "User",
+    site_admin: false,
+    name: null,
+    company: null,
+    blog: null,
+    location: null,
+    email: null,
+    hireable: null,
+    bio: null,
+    twitter_username: null,
+    public_repos: 0,
+    public_gists: 0,
+    followers: 0,
+    following: 0,
+    created_at: null,
+    updated_at: null
+  };
+
+  const [username, setUsername] = useState("hyagohoudini");
+  const [user, setUser] = useState(defautlUser);
+
+  useEffect(() => {
+    axios
+      .get(`https://api.github.com/users/${username}`)
+      .then((response) => {
+        const data = response.data;
+        if (data.login != null) {
+          setUser(data);
+        }
+      })
+      .catch((err) => {
+        setUser(defautlUser);
+        return;
+      });
+  }, [username]);
+
+  const roteamento = useRouter();
+
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    //Push pra outro endpoint
+    // window.location.href = '/chat';
+    // localStorage.setItem("username", `${username}`);
+    roteamento.push("/chat");
+  };
+
+  const handleErrBtn = () => {
+    alert("Git user must exist");
+  };
+
+  const handleGit = () => {
+    window.location.href = "https://github.com/signup?source=login";
+  };
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -100,6 +143,7 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={handleSubmit}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -129,12 +173,14 @@ export default function PaginaInicial() {
                 color: appConfig.theme.colors.neutrals["300"],
               }}
             >
-              Nome de usuário do github
+              Insert your GitHub Username
             </Text>
 
             {/* Campo de texto */}
             <TextField
               fullWidth
+              value={username}
+              onChange={handleChange}
               textFieldColors={{
                 neutral: {
                   textColor: appConfig.theme.colors.neutrals[200],
@@ -146,60 +192,121 @@ export default function PaginaInicial() {
             />
             {/* Campo de texto */}
 
-            <Button
-              type="submit"
-              label="Entrar"
-              fullWidth
-              buttonColors={{
-                contrastColor: appConfig.theme.colors.neutrals["000"],
-                mainColor: appConfig.theme.colors.primary[500],
-                mainColorLight: appConfig.theme.colors.primary[400],
-                mainColorStrong: appConfig.theme.colors.primary[600],
-              }}
-            />
+            {/* <input type="text" value = {username} onChange={handleChange} /> */}
+
+            {user.login != null ? (
+              <Button
+                type="submit"
+                label="Entrar"
+                fullWidth
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["000"],
+                  mainColor: appConfig.theme.colors.primary[500],
+                  mainColorLight: appConfig.theme.colors.primary[400],
+                  mainColorStrong: appConfig.theme.colors.primary[600],
+                }}
+              />
+            ) : (
+              <Button
+                onClick={handleErrBtn}
+                label="Entrar"
+                fullWidth
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["000"],
+                  mainColor: appConfig.theme.colors.neutrals[500],
+                  mainColorLight: appConfig.theme.colors.neutrals[400],
+                  mainColorStrong: appConfig.theme.colors.neutrals[600],
+                }}
+              />
+            )}
           </Box>
           {/* Formulário */}
 
           {/* Photo Area */}
-          <Box
-            styleSheet={{
-              marginLeft: "32px",
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              maxWidth: "200px",
-              padding: "16px",
-              backgroundColor: appConfig.theme.colors.neutrals[800],
-              border: "1px solid",
-              borderColor: appConfig.theme.colors.neutrals[999],
-              borderRadius: "10px",
-              flex: 1,
-              minHeight: "240px",
-            }}
-          >
-            <Image
+          {user.login != null ? (
+            <Box
               styleSheet={{
-                marginTop: "16px",
-
-                borderRadius: "50%",
-                marginBottom: "16px",
-              }}
-              src={`https://github.com/${username}.png`}
-            />
-
-            <Text
-              variant="body4"
-              styleSheet={{
-                color: appConfig.theme.colors.neutrals[200],
-                backgroundColor: appConfig.theme.colors.neutrals[900],
-                padding: "3px 10px",
-                borderRadius: "1000px",
+                marginLeft: "32px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                maxWidth: "200px",
+                padding: "16px",
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                border: "1px solid",
+                borderColor: appConfig.theme.colors.neutrals[999],
+                borderRadius: "10px",
+                flex: 1,
+                minHeight: "240px",
               }}
             >
-              {username}
-            </Text>
-          </Box>
+              <Image
+                styleSheet={{
+                  borderRadius: "50%",
+                  marginBottom: "16px",
+                }}
+                src={`https://github.com/${username}.png`}
+              />
+
+              <Text
+                variant="body4"
+                styleSheet={{
+                  color: appConfig.theme.colors.neutrals[200],
+                  backgroundColor: appConfig.theme.colors.neutrals[900],
+                  padding: "3px 10px",
+                  borderRadius: "1000px",
+                }}
+              >
+                {username}
+              </Text>
+            </Box>
+          ) : (
+            <Box
+              styleSheet={{
+                marginLeft: "32px",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                maxWidth: "200px",
+                padding: "16px",
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                border: "1px solid",
+                borderColor: appConfig.theme.colors.neutrals[999],
+                borderRadius: "10px",
+                flex: 1,
+                minHeight: "240px",
+              }}
+            >
+              <Text
+                variant="body2"
+                styleSheet={{
+                  marginTop: "20px",
+                  marginBottom: "45px",
+                  fontSize: "25px",
+                  color: appConfig.theme.colors.neutrals[200],
+                  backgroundColor: appConfig.theme.colors.neutrals[900],
+                  padding: "3px 10px",
+                  borderRadius: "10px",
+                }}
+              >
+                Não tem uma conta?
+              </Text>
+
+              <Button
+                label="Registrar no git"
+                onClick={handleGit}
+                fullWidth
+                buttonColors={{
+                  contrastColor: appConfig.theme.colors.neutrals["000"],
+                  mainColor: appConfig.theme.colors.primary[500],
+                  mainColorLight: appConfig.theme.colors.primary[400],
+                  mainColorStrong: appConfig.theme.colors.primary[600],
+                }}
+              />
+            </Box>
+          )}
           {/* Photo Area */}
+
           {/* QRcode Area */}
           <Box
             styleSheet={{
@@ -235,7 +342,7 @@ export default function PaginaInicial() {
                 borderRadius: "1000px",
               }}
             >
-              Repositório da aplicação no Git
+              Git repository
             </Text>
           </Box>
           {/* QRcode Area */}
